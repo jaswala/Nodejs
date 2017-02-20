9bc3953bd1fe91a9818874228f301099fd2b077d
Jaswal, Arshpreet <Arshpreet.Jaswal@cdk.com>
2017-02-20 13:36:21 -0800

    new exercise

---
+++ D12_ExportsVsModuleExports/Starter/app.js
@@ -0,0 +1,6 @@
+var greet = require('./greet');
+var greet2 = require('./greet2');
+var greet3 = require('./greet3');
+greet2.greet();
+greet3();
+
---
+++ D12_ExportsVsModuleExports/Starter/greet3.js
@@ -0,0 +1,6 @@
+var greet = function (){
+
+    console.log('Hello world');
+}
+
+module.exports = greet;
