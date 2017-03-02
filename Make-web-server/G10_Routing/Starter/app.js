3b64b94fda2d7b9b9f05a56894a3945fcc682e12
Arshpreet Jaswal <Arshpreet@Arshpreets-MacBook-Air.local>
2017-03-01 21:25:17 -0800

    new excerise

--- Make-web-server/G10_Routing/Starter/app.js
+++ Make-web-server/G10_Routing/Starter/app.js
@@ -3,11 +3,27 @@ var fs = require('fs');
 
 http.createServer(function (req, res) {
 
+  if(req.url === '/') {
+    fs.createReadStream(__dirname + '/index.htm').pipe
+    (res);
+  }
+
+ else if(req.url === '/api') {
+
   res.writeHead(200, {'Content-Type': 'application/json'});
   var obj = {
     firstname: 'John',
     lastname: 'Doe'
   };
   res.end(JSON.stringify(obj));
+}
 
-}).listen(8080, '127.0.0.1');
+ else { res.writeHead(404);
+  res.end();
+ }
+}).listen(1337, '127.0.0.1');
+
+
+// response to different url or requests
+//routing helps with it
+//look at url and give differnet respone to each requets.
