123ff6ef57eaf5e7d88e2bdbef4d462774e8e0ee
Jaswal, Arshpreet <Arshpreet.Jaswal@cdk.com>
2017-03-03 16:09:57 -0800

    new exercise

---
+++ Express/I1_InstallingExpressWebServer/Starter/app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.port || 3000;
+
+app.get('/', function(req,res) {
+    res.send('<html><head></head><body><h1>Hello world!!</h1></body></html>' );
+
+});
+
+app.get('/api', function(req,res) {
+
+    res.json({ firstname:'jason', lastname:'Doe'});
+});
+
+app.listen(port);
