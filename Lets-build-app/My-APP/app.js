a6fe8803e4c4ea30252938de9938516f53e13fc1
Arshpreet Jaswal <Arshpreet@Arshpreets-MacBook-Air.local>
2017-03-06 22:00:47 -0800

    new files

--- Lets-build-app/My-APP/app.js
+++ Lets-build-app/My-APP/app.js
@@ -3,6 +3,7 @@ var app = express();
 var mongoose = require('mongoose');
 var config = require('./config');
 var setupController = require('./controllers/setupController');
+var apiController = require('./controllers/apiController');
 
 //mongoose.connect('mongodb://test:test@ds025583.mlab.com:25583/nodetodosample-aj');
 
@@ -15,5 +16,5 @@ app.set('view engine','ejs');
 //mongoose.connect(config.getDbConnectionString());
 mongoose.connect('mongodb://test:test@ds025583.mlab.com:25583/nodetodosample-aj');
 setupController(app);
-
+apiController(app);
 app.listen(port);
